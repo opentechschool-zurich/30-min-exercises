2aa867b25bbb7f8f7271c7dadd5a896fbca3a15a
ale rimoldi <ale@graphicslab.org>
2024-03-19 22:01:46 +0100

    use shift

--- count-ones/ale/fancy.js
+++ count-ones/ale/fancy.js
@@ -2,7 +2,8 @@ const countOnes = (number) => {
   let count = 0;
   while (number > 0) {
     count += number & 1; // is the last bit a 1
-    number /= 2; // remove the last bit (... it's a power of two)
+    number >>= 1; // shift by 1
+    // number /= 2; // remove the last bit (... it's a power of two)
   }
   return count;
 }
