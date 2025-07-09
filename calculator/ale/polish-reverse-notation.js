f1a004a0734f82d30a16e361ee84d238d11ac41d
ale rimoldi <ale@graphicslab.org>
2025-07-09 21:20:42 +0200

    correctly implement the postfix calculation

--- calculator/ale/polish-reverse-notation.js
+++ calculator/ale/polish-reverse-notation.js
@@ -3,15 +3,74 @@
 //   operator is encountered, perform the calculation and replace the values from the list
 //   with the result.
 
+/**
+ * A very simplistic function, that only correctly process very simple cases
+ */
+function calculate_simple(calculation) {
+  while (calculation.length > 1) {
+    calculation.splice(0, 3, calculation[2](calculation[0], calculation[1]));
+  }
+  return calculation[0];
+}
+
+/**
+ * Convert a post fix calculation string into tokens: numebrs and operator functions
+ */
+function tokenize(calculation, operators) {
+  tokens = [];
+  value = ''
+  for (const c of calculation) {
+    if (c >= '0' && c <= '9') {
+      value += c;
+      continue;
+    }
+    if (value.length > 0) {
+      tokens.push(Number(value));
+      value = ''
+    }
+    if (c == ' ') {
+      continue;
+    }
+    if (operators.hasOwnProperty(c)) {
+      tokens.push(operators[c])
+    }
+    // invalid character: ignore it
+  }
+  return tokens;
+}
+
+/**
+ * Perform a postfix from a list of values and operators
+ */
+function calculate(calculation) {
+  let operands = [];
+  while (calculation.length > 1) {
+    let operand = calculation.shift();
+    while (!(operand instanceof Function)) {
+      operands.push(operand);
+      operand = calculation.shift();
+    }
+    let [a, b] = operands.splice(-2, 2);
+    calculation.unshift(operand(a, b));
+  }
+  return calculation[0];
+}
+
 sum = (a, b) => a + b
 mul = (a, b) => a * b
 sub = (a, b) => a - b
 div = (a, b) => a / b
 
-calculation = [3, 2, sum, 4, mul]
+console.assert(calculate_simple([3, 2, sum, 4, mul]) == 20);
 
-while (calculation.length > 1) {
-  calculation.splice(0, 3, calculation[2](calculation[0], calculation[1]))
+console.assert(calculate([3, 2, sum, 4, mul]) == 20);
+console.assert(calculate([3, 4, sum, 5, 6, sum, mul]) == 77);
+
+let operators = {
+  '+': sum,
+  '-': sub,
+  '*': mul,
+  '/': div,
 }
 
-console.log(calculation[0])
+console.assert(calculate(tokenize('3 4 + 5 6 + *', operators)) == 77);
