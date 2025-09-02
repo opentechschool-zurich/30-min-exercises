bd4efdfa0919ba2ae4fb6c154d506bcf28581aeb
ale rimoldi <ale@graphicslab.org>
2025-09-02 20:42:12 +0200

    ots solution

--- rainwater-trapping/index.js
+++ rainwater-trapping/index.js
@@ -3,27 +3,42 @@ function get_water_quantity(blocks) {
     return 0;
   }
   let volume = 0;
-  let max_height = 0;
   let max_i = 0;
-  for (let i = 1; i < blocks.length; i++) {
-    if (blocks[0] <= blocks[i]) {
-      max_i = i;
-      max_height = blocks[i];
-      break;
-    } else if (blocks[i] >= max_height) {
-      max_height = blocks[i];
-      max_i = i;
+  
+  while (max_i < blocks.length - 1) {
+    let max_height = 0;
+    start_i = max_i
+    
+    for (let i = start_i + 1; i < blocks.length; i++) {
+      if (blocks[start_i] < blocks[i]) {
+        max_i = i;
+        max_height = blocks[i];
+        break;
+      } else if (blocks[i] >= max_height) {
+        max_height = blocks[i];
+        max_i = i;
+      }
     }
+    let rain_height = Math.min(blocks[0], blocks[max_i]);
+    // console.log(rain_height, max_i);
+    for (let i = start_i; i < max_i; i++) {
+      volume += Math.max(0, rain_height - blocks[i]);
+    }
+
+    // console.log(volume);
+    
   }
-  let rain_height = Math.min(blocks[0], blocks[max_i]);
-  console.log(rain_height, max_i);
-  for (let i = 0; i < max_i; i++) {
-    volume += Math.max(0, rain_height - blocks[i]);
-  }
-  console.log(volume);
-  return 0;
+
+  return volume;
 }
 
-console.log(get_water_quantity( [3, 0, 1, 0, 4, 0, 2]) == 10);
-// console.log(get_water_quantity( [3, 0, 2, 0, 4]) == 7);
-// console.log(get_water_quantity( [1, 2, 3, 4]) == 0);
+console.assert(get_water_quantity([3, 0, 1, 0, 4, 0, 2]) == 10);
+console.assert(get_water_quantity( [3, 0, 2, 0, 4]) == 7);
+console.assert(get_water_quantity( [1, 2, 3, 4]) == 0);
+console.assert(get_water_quantity( [4, 3, 2, 1]) == 0);
+console.assert(get_water_quantity( [4, 0, 4, 0, 4]) == 8);
+console.assert(get_water_quantity( [4, 0, 4, 0, 0]) == 4);
+console.assert(get_water_quantity( [0, 0, 0, 0, 1]) == 0);
+console.assert(get_water_quantity( [1, 0, 0, 0, 0]) == 0);
+
+// https://www.geeksforgeeks.org/dsa/trapping-rain-water/
